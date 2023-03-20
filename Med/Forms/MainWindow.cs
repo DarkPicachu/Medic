d174e669399f5e6dc60f2952d81a097fb15ea940
sonya <sonya@>
2023-03-20 13:21:47 +0700

    Минификсы говна

--- Med/Forms/MainWindow.cs
+++ Med/Forms/MainWindow.cs
@@ -14,102 +14,167 @@ using System.Reflection;
 
 namespace Med.Forms
 {
+
     
     public partial class MainWindow : Form
     {
+       
+
         DataGridView dgw = new DataGridView();
-        
+
         DataBase dataBase = new DataBase();
         string queryString = "";
         public MainWindow(int role)
         {
             InitializeComponent();
-            CreateColumns(dataGridView1,1);
+            CreateCollumns();
             RefresDataGrid(dataGridView1, 1);
             searchtags(1);
 
-            dgw = dataGridView1;            
+            dgw = dataGridView1;
             if (role == 1)
                 return;
 
-            tabAccounts.Parent = null;
-            tabWorkers.Parent = null;
-            tabRole.Parent = null;
-            tabKab.Parent = null;
-            tabDoljnosti.Parent = null;
+            tab5.Parent = null;
+            tab4.Parent = null;
+            tab7.Parent = null;
+            tab8.Parent = null;
+            tab6.Parent = null;
 
         }
-        
-        public void accNames (string log, string pas)
+
+        public void accNames(string log, string pas)
         {
-            queryString = $"select w.name +' '+w.surname from workers w, accounts acc where (acc.login + acc.password = '" + log + pas + "') and acc.idaccounts = w.account";
+            queryString = $"select w.name +' '+w.surname from workers w, accounts acc where (acc.login + acc.password = '" + log + pas + "') and acc.id = w.account";
 
             SqlCommand command = new SqlCommand(
                 queryString, dataBase.getConnection());
-            
+
 
             dataBase.openConnection();
             SqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
                 accName.Text = reader.GetString(0);
-            
+
             reader.Close();
+        }
+
+        public void CreateCollumns()
+        {
+            for (int i=1; i<=tabControl1.TabCount; i++)
+            {
+                DataGridView dgw = new DataGridView();
+                dgw = (tabControl1.Controls["tab" + i] as TabPage).Controls["dataGridView" + i] as DataGridView;
+
+                switch (i)
+                {
+                    case 1:
+                        dgw.Columns.Add("id", "id");
+                        dgw.Columns.Add("client", "client");
+                        dgw.Columns.Add("worker", "worker");
+                        dgw.Columns.Add("time", "time");
+                        dgw.Columns.Add("diagnoz", "diagnoz");
+                        dgw.Columns.Add("healing", "healing");
+                        dgw.Columns.Add("rest", "rest");
+                        break;
+                    case 2:
+                        dgw.Columns.Add("id", "id");
+                        dgw.Columns.Add("Name", "Name");
+                        dgw.Columns.Add("Surname", "Surname");
+                        dgw.Columns.Add("phone", "phone");
+                        dgw.Columns.Add("Age", "Age");
+                        break;
+                    case 3:
+                        dgw.Columns.Add("id", "id");
+                        dgw.Columns.Add("diagnoz", "diagnoz");
+                        break;
+                    case 4:
+                        dgw.Columns.Add("id", "id");
+                        dgw.Columns.Add("name", "name");
+                        dgw.Columns.Add("surname", "surname");
+                        dgw.Columns.Add("name", "name");
+                        dgw.Columns.Add("cabinet", "cabinet");
+                        dgw.Columns.Add("account", "account");
+                        break;
+                    case 5:
+                        dgw.Columns.Add("id", "id");
+                        dgw.Columns.Add("login", "Логин");
+                        dgw.Columns.Add("password", "Пароль");
+                        dgw.Columns.Add("rol", "Роль");
+                        break;
+                    case 6:
+                        dgw.Columns.Add("id", "id");
+                        dgw.Columns.Add("name", "name");
+                        break;
+                    case 7:
+                        dgw.Columns.Add("id", "id");
+                        dgw.Columns.Add("rols", "rols");
+                        break;
+                    case 8:
+                        dgw.Columns.Add("id", "id");
+                        dgw.Columns.Add("rang", "rang");
+                        break;
+
+
+                }
+            }
+
         }
 
         private void CreateColumns(DataGridView dgw, int index)
         {
-            dgw.Columns.Clear();
+           /* dgw.Columns.Clear();
             switch (index)
-            { 
+            {
                 case 1:
                     dgw.Columns.Add("id", "id");
-                    dgw.Columns.Add("client", "Клиент");
-                    dgw.Columns.Add("worker", "Лечащий врач");
-                    dgw.Columns.Add("time", "Дата прихода");
-                    dgw.Columns.Add("diagnoz", "Диагноз");
-                    dgw.Columns.Add("healing", "Лечение");
-                    dgw.Columns.Add("rest", "Отдых");
+                    dgw.Columns.Add("client", "client");
+                    dgw.Columns.Add("worker", "worker");
+                    dgw.Columns.Add("time", "time");
+                    dgw.Columns.Add("diagnoz", "diagnoz");
+                    dgw.Columns.Add("healing", "healing");
+                    dgw.Columns.Add("rest", "rest");
                     break;
                 case 2:
                     dgw.Columns.Add("id", "id");
-                    dgw.Columns.Add("name", "Имя");
-                    dgw.Columns.Add("surname", "Фамилия");
-                    dgw.Columns.Add("phone", "Номер телефона");
-                    dgw.Columns.Add("age", "Возраст");
-                    break; 
+                    dgw.Columns.Add("Name", "Name");
+                    dgw.Columns.Add("Surname", "Surname");
+                    dgw.Columns.Add("phone", "phone");
+                    dgw.Columns.Add("Age", "Age");
+                    break;
                 case 3:
                     dgw.Columns.Add("id", "id");
-                    dgw.Columns.Add("diagnoz", "Диагноз");
-                    break; 
+                    dgw.Columns.Add("diagnoz", "diagnoz");
+                    break;
                 case 4:
                     dgw.Columns.Add("id", "id");
-                    dgw.Columns.Add("name", "Имя");
-                    dgw.Columns.Add("surname", "Фамилия");
-                    dgw.Columns.Add("rang", "Должность");
-                    dgw.Columns.Add("number", "Кабинет");
-                    dgw.Columns.Add("log_pas", "Логин и пароль");
+                    dgw.Columns.Add("name", "name");
+                    dgw.Columns.Add("surname", "surname");
+                    dgw.Columns.Add("name", "name");
+                    dgw.Columns.Add("cabinet", "cabinet");
+                    dgw.Columns.Add("account", "account");
                     break;
                 case 5:
                     dgw.Columns.Add("id", "id");
                     dgw.Columns.Add("login", "Логин");
                     dgw.Columns.Add("password", "Пароль");
                     dgw.Columns.Add("rol", "Роль");
-                    break;  
+                    break;
                 case 6:
                     dgw.Columns.Add("id", "id");
-                    dgw.Columns.Add("rang", "Должность");
-                    break;  
+                    dgw.Columns.Add("name", "name");
+                    break;
                 case 7:
                     dgw.Columns.Add("id", "id");
-                    dgw.Columns.Add("rol", "Роль");
+                    dgw.Columns.Add("rols", "rols");
                     break;
                 case 8:
                     dgw.Columns.Add("id", "id");
-                    dgw.Columns.Add("rang", "Должность");
+                    dgw.Columns.Add("rang", "rang");
                     break;
 
 
-            }
+            }*/
 
 
         }
@@ -119,13 +184,13 @@ namespace Med.Forms
             switch (index)
             {
                 case 1:
-                    dgw.Rows.Add(record.GetInt32(0), record.GetString(1), record.GetString(2), record.GetDateTime(3).ToString("d"), record.GetString(4) ,record.GetString(5), record.GetString(6));
+                    dgw.Rows.Add(record.GetInt32(0), record.GetString(1), record.GetString(2), record.GetDateTime(3).ToString("d"), record.GetString(4), record.GetString(5), record.GetString(6));
                     break;
                 case 2:
                     dgw.Rows.Add(record.GetInt32(0), record.GetString(1), record.GetString(2), record.GetString(3), record.GetInt32(4));
                     break;
                 case 3:
-                    dgw.Rows.Add(record.GetInt32(0), record.GetString(1));                    
+                    dgw.Rows.Add(record.GetInt32(0), record.GetString(1));
                     break;
                 case 4:
                     dgw.Rows.Add(record.GetInt32(0), record.GetString(1), record.GetString(2), record.GetString(3), record.GetInt32(4), record.GetString(5));
@@ -147,53 +212,61 @@ namespace Med.Forms
 
         private void RefresDataGrid(DataGridView dgw, int table_index)
         {
-            
+
             dgw.Rows.Clear();
             switch (table_index)
             {
                 case 1:
-                    queryString = $"select j.idjornal , (c.Name+ ' ' + c.Surname),  (w.name+' '+w.surname), j.time, d.diagnoz , j.healing, j.rest\r\nfrom client c, workers w, jornal j , Diagnoz d\r\nwhere c.id = j.client and w.idworker = j.worker and d.iddiaonoz = j.diagnoz";
-                    break; 
+                    queryString = $"select j.id , (c.Name+ ' ' + c.Surname),  (w.name+' '+w.surname), j.time, d.diagnoz , j.healing, j.rest\r\nfrom client c, workers w, jornal j , Diagnoz d\r\nwhere c.id = j.client and w.id = j.worker and d.id = j.diagnoz";
+                    break;
                 case 2:
-                    queryString = $"select c.id , c.Name, c.Surname ,c.phone ,c.Age ,  (ac.login +';' +ac.password)\r\nfrom client c,  accounts ac\r\nwhere   ac.idaccounts = c.id";
-                    break; 
+                    queryString = $"select c.id , c.Name, c.Surname ,c.phone ,c.Age ,  (ac.login +';' +ac.password)\r\nfrom client c,  accounts ac\r\nwhere   ac.id = c.id";
+                    break;
                 case 3:
-                    queryString = $"select d.iddiaonoz, d.diagnoz\r\nfrom Diagnoz d";                    
-                    break; 
+                    queryString = $"select d.id, d.diagnoz\r\nfrom Diagnoz d";
+                    break;
                 case 4:
-                    queryString = $"select wr.idworker, wr.name, wr.surname, r.name, cab.number,  ac.login+ ';'+ac.password\r\nfrom workers wr, cabinets cab, Rang r,accounts ac\r\nwhere  ac.idaccounts =wr.idworker and r.idrang = cab.rang and cab.number = wr.cabinet";
-                    break; 
+                    queryString = $"select wr.id, wr.name, wr.surname, r.name, cab.id,  ac.login+ ';'+ac.password\r\nfrom workers wr, cabinets cab, Rang r,accounts ac\r\nwhere  ac.id =wr.id and r.id = cab.rang and cab.id = wr.cabinet";
+                    break;
                 case 5:
-                    queryString = $"select ac.idaccounts, ac.login, ac.password, rl.rols\r\nfrom accounts ac, rols rl\r\nwhere rl.idrols = ac.idaccounts";
-                    break; 
+                    queryString = $"select ac.id, ac.login, ac.password, rl.rols\r\nfrom accounts ac, rols rl\r\nwhere rl.id = ac.id";
+                    break;
                 case 6:
-                    queryString = $"select r.idrang, r.name\r\nfrom Rang r";
+                    queryString = $"select r.id, r.name\r\nfrom Rang r";
                     break;
                 case 7:
-                    queryString = $"select rl.idrols , rl.rols\r\nfrom rols rl";
+                    queryString = $"select rl.id , rl.rols\r\nfrom rols rl";
                     break;
                 case 8:
-                    queryString = $"select cb.number , r.name\r\nfrom cabinets cb, Rang r\r\nwhere r.idrang = cb.rang";
+                    queryString = $"select cb.id , r.name\r\nfrom cabinets cb, Rang r\r\nwhere r.id = cb.rang";
                     break;
             }
 
-            
+
             SqlCommand command = new SqlCommand(
-                queryString,dataBase.getConnection());
+                queryString, dataBase.getConnection());
 
 
             dataBase.openConnection();
             SqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
-                ReadSingleRow(dgw,reader,table_index);
+                ReadSingleRow(dgw, reader, table_index);
             }
             reader.Close();
-        }        
+        }
 
         private void butSearch_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void search()
+        {
+
+            SqlCommand command = new SqlCommand(
+                queryString, dataBase.getConnection());
+            command.ExecuteNonQuery();
         }
 
         private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
@@ -205,32 +278,32 @@ namespace Med.Forms
         {
             searchPole.Items.Clear();
             searchPole.Text = "";
-            List<string> tags= new List<string> { };
+            List<string> tags = new List<string> { };
             switch (index)
             {
                 case 1:
-                    tags= new List<string> { "id", "Клиент", "Лечащий врач", "Дата прихода", "Диагноз", "Лечение", "Отдых" };
+                    tags = new List<string> { "id", "Клиент", "Лечащий врач", "Дата прихода", "Диагноз", "Лечение", "Отдых" };
                     break;
                 case 2:
-                    tags = new List<string> { "id", "Имя", "Фамилия", "Номер телефона", "Возраст"};
+                    tags = new List<string> { "id", "Имя", "Фамилия", "Номер телефона", "Возраст" };
                     break;
                 case 3:
-                    tags = new List<string> { "id", "Диагноз"};
+                    tags = new List<string> { "id", "Диагноз" };
                     break;
                 case 4:
-                    tags = new List<string> { "id", "Имя", "Фамилия", "Должность", "Кабинет", "Логин и пароль"};
+                    tags = new List<string> { "id", "Имя", "Фамилия", "Должность", "Кабинет", "Логин и пароль" };
                     break;
                 case 5:
-                    tags = new List<string> { "id", "Логин", "Пароль", "Роль" };    
+                    tags = new List<string> { "id", "Логин", "Пароль", "Роль" };
                     break;
                 case 6:
-                    tags = new List<string> { "id", "Должность"};
+                    tags = new List<string> { "id", "Должность" };
                     break;
                 case 7:
-                    tags = new List<string> { "id", "Роль"};
+                    tags = new List<string> { "id", "Роль" };
                     break;
                 case 8:
-                    tags = new List<string> { "id", "Должность"};
+                    tags = new List<string> { "id", "Должность" };
                     break;
             }
             string[] tagsstring = tags.ToArray();
@@ -240,10 +313,12 @@ namespace Med.Forms
 
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
-            int index= tabControl1.SelectedIndex+1;
-            
-            switch (index)
+
+            int index = tabControl1.SelectedIndex + 1;
+
+            dgw = (tabControl1.Controls["tab" + index] as TabPage).Controls["dataGridView" + index] as DataGridView;
+           
+            /*switch (index)
             {
                 case 1:
                     dgw = dataGridView1;
@@ -269,17 +344,17 @@ namespace Med.Forms
                 case 8:
                     dgw = dataGridView8;
                     break;
-            }
-            
-            CreateColumns(dgw, index);
-            RefresDataGrid( dgw, index);
+            }*/
+
+            RefresDataGrid(dgw, index);
             searchtags(index);
         }
 
+
         private void butDel_Click(object sender, EventArgs e)
         {
-            int index = tabControl1.SelectedIndex +1;
-            int id = Convert.ToInt32(dgw[0,dgw.CurrentRow.Index].Value.ToString());           
+            int index = tabControl1.SelectedIndex + 1;
+            int id = Convert.ToInt32(dgw[0, dgw.CurrentRow.Index].Value.ToString());
             DialogResult result = MessageBox.Show(
                 "Вы действительно хотите удалить запись?",
                 "Удаление записи",
@@ -288,45 +363,46 @@ namespace Med.Forms
                 MessageBoxDefaultButton.Button2,
                 MessageBoxOptions.DefaultDesktopOnly);
 
-                if (result == DialogResult.Yes)
+            if (result == DialogResult.Yes)
             {
-                deleteRow(index,id);
-                RefresDataGrid( dgw, index);
+                deleteRow(index, id);
+                RefresDataGrid(dgw, index);
             }
 
             this.TopMost = true;
         }
         private void deleteRow(int index, int id)
         {
-            
+            string table_name = "";
 
             switch (index)
             {
                 case 1:
-                    queryString = $"delete from workers where(idjornal = " +id+")";
+                    table_name = "journal";
                     break;
                 case 2:
-                    queryString = $"delete from workers where(id = " + id + ")";
+                    table_name = "client";
                     break;
                 case 3:
-                    queryString = $"delete from workers where(iddiaonoz = " + id + ")";
+                    table_name = "Diagnoz";
                     break;
                 case 4:
-                    queryString = $"delete from workers where(idworker = " + id + ")";
+                    table_name = "workers";
                     break;
                 case 5:
-                    queryString = $"delete from workers where(idaccounts = " + id + ")";
+                    table_name = "accounts";                    
                     break;
                 case 6:
-                    queryString = $"delete from workers where(idrang = " + id + ")";
+                    table_name = "Rang";
                     break;
-                case 7:
-                    queryString = $"delete from workers where(idrols = " + id + ")";
+                case 7:                    
+                    table_name = "rols";
                     break;
                 case 8:
-                    queryString = $"delete from workers where(number = " + id + ")";
-                    break;
+                    table_name = "cabinets";
+                    break;  
             }
+            queryString = $"delete from {table_name} where id = '{id}' ";
             SqlCommand command = new SqlCommand(
                 queryString, dataBase.getConnection());
             command.ExecuteNonQuery();
